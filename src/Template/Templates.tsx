6a70a8890d940d1c1e0e09c647c156df5d715518
Qrchy <130994567+Qrchy@users.noreply.github.com>
2024-09-28 23:22:10 +0200

    templates fixed

--- src/Template/Templates.tsx
+++ src/Template/Templates.tsx
@@ -4,37 +4,36 @@ function Templates()
 {
     return (
         <>
-            <div id="creator">
-                <h1>Create quiz</h1>
-                <form>
-                    <table>
-                        
-                        <tr>
-                            <th>Content: </th>
-                            <td id="con"><textarea placeholder="You can write some informational text here or paste larger articles." id="cont" name="content" rows={2} cols={30}></textarea></td>
-                        </tr>
-                    </table>
+            <div id="all">
+                <div id="creator">
+                    <h1 className='title01'>Create quiz</h1>
+                    <form>
+                        <table className='tab2'>
 
-                <h4>Configuration:</h4>
-                <p className='config'>
-                    Number of questions: 
-                    <input type="number" max={128} min={2} step={2}></input>
-                </p>
-                <p className='config'>
-                    Difficulty: 
-                    <select>
-                        <option value="easy">Easy</option>
-                        <option value="medium">Medium</option>
-                        <option value="hard">Hard</option>
-                    </select>
-                </p>
-                    <button><span id="butt">Wygeneruj quiz</span></button>
-                </form>
-                
-            </div>
-            <div id="result">
-                <h3>Result</h3>
-                <p id="res">Pytanie1</p>
+                            <tr>
+                                <th className='tab1'>Content: </th>
+                                <td className='con'><textarea placeholder="You can write some informational text here or paste larger articles." id="cont" name="content" rows={2} cols={30}></textarea></td>
+                            </tr>
+                        </table>
+                    <div id="configure">
+                        <h4 className='config1'>Configuration:</h4>
+                        <p className='config'>
+                            Number of questions: 
+                            <input type="number" max={128} min={2} step={2} className='num1'></input>
+                        </p>
+                        <p className='config'>
+                            Difficulty: 
+                            <select className='sel1'>
+                                <option value="easy">Easy</option>
+                                <option value="medium">Medium</option>
+                                <option value="hard">Hard</option>
+                            </select>
+                        </p>
+                        <button className='generate'><span id="butt">Wygeneruj quiz</span></button>
+                    </div>
+                    </form>
+
+                </div>
             </div>
         </>
     )
