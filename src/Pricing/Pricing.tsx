99c7f94df797399ba1ab753159a52e4a83d219f9
patrykduda2005 <patrykduda7@gmail.com>
2024-09-29 08:04:59 +0200

    CENY

--- src/Pricing/Pricing.tsx
+++ src/Pricing/Pricing.tsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 function Benefity() {
     return (
         <>
-            <span>Plusy ktore </span>
+            <h1>Plusy ktore </h1>
+            <div>
             <ul>
                 <li>Dupa andrzeja</li>
                 <li>TANIA CENA</li>
@@ -12,9 +13,23 @@ function Benefity() {
                 <li>TANIE JAK U PIOTRA</li>
                 <li>99,999999% uptime</li>
             </ul>
+            </div>
         </>
     )
 }
+
+function THEPRICE({cena}) {
+    return (
+        <>
+            <p id="cena">
+                <span class="star">⭐</span>
+                {cena}zł
+                <span class="star">⭐</span>
+            </p>
+        </>
+    )
+}
+
 
 
 function Pricing() {
@@ -33,12 +48,19 @@ function Pricing() {
         <main>
         <div id="pricing">
             <Benefity />
-            <p>Cena za miesiac: {cena}zł</p>
             <span>Wymiar: </span>
             <select onChange={oblicz}>
+                <option disabled selected value> -- WYBIERZ --</option>
                 <option value="month">Miesięczny</option>
                 <option value="roczny">Roczny</option>
             </select>
+            <hr/>
+            <p>Cena za miesiac: </p>
+            {
+                cena != "0" ?
+                    <THEPRICE cena={cena}/>
+                : ""
+            }
         </div>
         </main>
     )
