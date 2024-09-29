c14dd3964019eed929e3de85555f20d8e653264b
patrykduda2005 <patrykduda7@gmail.com>
2024-09-29 02:45:23 +0200

    home page calkiem calkiem

--- src/HomePage/MainHomePage.tsx
+++ src/HomePage/MainHomePage.tsx
@@ -1,4 +1,5 @@
-//import './MainHomePageCSS.css';
+import './MainHomePageCSS.css';
+import '../srodekCSS.css'
 import ShortAppInfo from './ShortAppInfo';
 import Image from './Image';
 import Info from './Info';
@@ -8,12 +9,14 @@ function MainHomePage({setPage}) {
 
     return (
       <div id='MainPage'>
+      <main>
         <ShortAppInfo/>
         
         <button onClick={()=>{setPage(Page.AccountPage)}} id='MainPageA1'>Zaloguj się</button>
 
         <Image img={'./cos.png'}/>
         <Info/>
+      </main>
       </div>
     )
   }
