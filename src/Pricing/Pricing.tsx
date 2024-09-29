02345d569b9902be2560eac177ee05719164150a
patrykduda2005 <patrykduda7@gmail.com>
2024-09-29 05:57:20 +0200

    prosty pricing

--- src/Pricing/Pricing.tsx
+++ src/Pricing/Pricing.tsx
@@ -1,10 +1,31 @@
 import './Pricing.css'
+import '../srodekCSS.css'
+import { useState } from 'react';
+
 
 function Pricing() {
+    let [cena, setCena] = useState("0");
+    let oblicz = (e) => {
+        switch (e.target.value) {
+            case "month":
+                setCena("19.99")
+                break;
+            case "roczny":
+                setCena("7.70")
+        }
+    }
+
     return (
+        <main>
         <div id="pricing">
-            <img src="kahoot.png"/>
+            <p>Cena za miesiac: {cena}zł</p>
+            <span>Wymiar: </span>
+            <select onChange={oblicz}>
+                <option value="month">Miesięczny</option>
+                <option value="roczny">Roczny</option>
+            </select>
         </div>
+        </main>
     )
 }
 
